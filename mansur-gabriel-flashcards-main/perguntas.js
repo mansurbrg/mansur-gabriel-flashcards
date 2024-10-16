6d5e7065081e1cdd5a471e323de4451da7516205
mansurbrg <133408107+mansurbrg@users.noreply.github.com>
2024-10-16 16:00:23 -0300

    Add files via upload

---
+++ mansur-gabriel-flashcards-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
